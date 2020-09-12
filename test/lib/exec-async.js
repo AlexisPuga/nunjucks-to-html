e7941d95337d12887b91de5166d15adad25ea74e
Alexis Puga Ruíz <alexispuga@pm.me>
2020-09-11 22:14:17 -0500

    BUG: Fix "nunjucks-to-html is not a command"
    Call the command using the path to its file in lib/exec-async.js

--- test/lib/exec-async.js
+++ test/lib/exec-async.js
@@ -1,10 +1,18 @@
 const {exec} = require('child_process');
+const path = require('path');
 
 async function execAsync (command, options) {
 
 	return await new Promise((resolve, reject) => {
 
-		exec(command, options, (error, stdout, stderr) => {
+		const pathToCommand = (/^nunjucks-to-html/.test(command)
+			? path.join(process.cwd(), 'bin', command.replace('nunjucks-to-html', 'cli.js'))
+			: null
+		);
+
+		if (!pathToCommand) { return void reject(new Error('Unknown command.')); }
+
+		exec(pathToCommand, options, (error, stdout, stderr) => {
 
 			if (error) { return reject(error); }
 
