33f6abd4a7677fcb3e802d8eee5495409630f431
Alexis Puga Ruíz <alexispuga@pm.me>
2020-09-12 03:45:54 -0500

    MAINT: Use promisify on lib/exec-async.js

--- test/lib/exec-async.js
+++ test/lib/exec-async.js
@@ -1,27 +1,19 @@
 const {exec} = require('child_process');
+const {promisify} = require('util');
 const path = require('path');
+const execPromisified = promisify(exec);
 const projectRoot = path.resolve(__dirname, '..', '..');
 
 async function execAsync (command, options) {
 
-	return await new Promise((resolve, reject) => {
+	const pathToCommand = (/^nunjucks-to-html/.test(command)
+		? path.resolve(projectRoot, 'bin', command.replace('nunjucks-to-html', 'cli.js'))
+		: null
+	);
 
-		const pathToCommand = (/^nunjucks-to-html/.test(command)
-			? path.resolve(projectRoot, 'bin', command.replace('nunjucks-to-html', 'cli.js'))
-			: null
-		);
+	if (!pathToCommand) { throw new Error('Unknown command.'); }
 
-		if (!pathToCommand) { return void reject(new Error('Unknown command.')); }
-
-		exec(pathToCommand, options, (error, stdout, stderr) => {
-
-			if (error) { return reject(error); }
-
-			resolve({stderr, stdout});
-
-		});
-
-	});
+	return await execPromisified(pathToCommand, options);
 
 }
 
